3f0c8f972328cf7ca8d74624a4bcda9c1b882929
Scott Behrens <sbehrens@gmail.com>
2015-08-26 14:18:29 -0700

    removed sslify

--- setup.py
+++ setup.py
@@ -30,7 +30,6 @@ setup(
         'Flask-RESTful==0.3.4',
         'Flask-SQLAlchemy==2.0',
         'Flask-Script==2.0.5',
-        'Flask-SSLify==0.1.5',
         'Flask-WTF==0.11',
         'Pillow==2.8.2',
         'SQLAlchemy==1.0.5',
